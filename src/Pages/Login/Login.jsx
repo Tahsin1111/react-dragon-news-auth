9818c05de32cceaeb79862ac0f8266415305da4b
Tahsin1111 <entimojumder@gmail.com>
2023-10-09 01:27:11 +0600

    Register page with form data to collect data

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+    }
+    return (
+        <div>
+            <Navbar></Navbar>
+          <div>
+          <h2 className="text-3xl my-10 text-center">This is login</h2>
+            <div className="card flex-shrink-0 md:w-3/4 lg:1/2 max-auto shadow-2xl bg-base-100">
+      <form onSubmit={handleLogin} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="Email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="Password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+      <p className="text-center">Dont have an account ? <Link className="text-blue-600 font-bold" to="/register">Register</Link> </p>
+    </div>
+          </div>
+
+
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+    }
+    return (
+        <div>
+        <Navbar></Navbar>
+      <div>
+      <h2 className="text-3xl my-10 text-center">This is Register</h2>
+        <div className="card flex-shrink-0 md:w-3/4 lg:1/2 max-auto shadow-2xl bg-base-100">
+  <form onSubmit={handleRegister} className="card-body">
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Name</span>
+      </label>
+      <input type="text" placeholder="Name" className="input input-bordered" />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Photo URL</span>
+      </label>
+      <input type="text" placeholder="Photo URL" className="input input-bordered" />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Email</span>
+      </label>
+      <input type="email" placeholder="Email" className="input input-bordered" required />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Password</span>
+      </label>
+      <input type="password" placeholder="Password" className="input input-bordered" required />
+      <label className="label">
+        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+      </label>
+    </div>
+    <div className="form-control mt-6">
+      <button className="btn btn-primary">Login</button>
+    </div>
+  </form>
+  <p className="text-center">Already have an account ? <Link className="text-blue-600 font-bold" to="/login">Login</Link> </p>
+</div>
+      </div>
+
+
+    </div>
+    );
+};
+
+export default Register;
